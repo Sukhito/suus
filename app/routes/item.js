1eeaa33741f35e3117526e087cf532261cd596b5
sukhito <sukhito.tanawal@gmail.com>
2017-06-26 10:48:01 +0800

    not sure

--- app/routes/item.js
+++ app/routes/item.js
@@ -2,9 +2,16 @@ var Item = require('../model/item');
 
 module.exports = function(app){
     app.get('/api/items',function(req,res){
-        Item.find({status : null},null,{sort:{name:1}},function(err,items){
+        Item.find({status : null},"name dimension modal prices ketjual",{sort:{name:1}},function(err,items){
             if(err) res.send(err);
             res.json(items);
+        })
+    })
+
+	app.get('/api/items/:id',function(req,res){
+        Item.findOne({_id:req.params.id},function(err,item){
+            if(err) res.send(err);
+            res.json(item);
         })
     })
 }
